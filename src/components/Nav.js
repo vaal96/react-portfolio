46fab58fb773074c3aff8a41de44f87803905ff7
Valeria Aguilar <valeriaaguilar@live.com>
2021-11-23 09:52:03 -0800

    deply

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+const Nav = ()=>{
+    return <nav className="nav nav-pills nav-fill text-pink ">
+        
+    <a className="nav-link" href="https://www.linkedin.com/in/vaal">LinkedIn</a>
+    <a className="nav-link" href="https://github.com/vaal96">Github</a>
+    <a className="nav-link" href="mailto: valeriaaguilar@live.com">Email</a>
+  </nav>
+}
+
+export default Nav
